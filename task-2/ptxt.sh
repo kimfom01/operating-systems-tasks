f4f92558f90bbdacb446bac655c4d37f2e7f6fb4
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-07 17:53:01 +0300

    Keep track of the arguments and move loop outside the case statement

--- task-2/ptxt.sh
+++ task-2/ptxt.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
+if [ "$#" != 6 ]
+then
+    echo "Invalid argument"
+    echo "usage bash $0 -n <number of text output> -t <output timeout> -- <text to output>"
+    exit 1
+fi
+
 text="$6"
+
 while [ -n "$1" ]
 do
-	case "$1" in
-	-t)
-		sleep "$2"
-		shift 2
-		;;
-	-n)
-		for i in `seq "$2"`
-		do
-			echo "$text"
-		done
-		shift 2
-		;;
-	--)
-		shift
-		break
-		;;
-	*)
-		echo "Invalid argument"
-		echo "usage bash $0 -n <number of text output> -t <output timeout> -- <text to output>"
-		exit 1
-		;;
-	esac
+    case "$1" in
+        -n)
+            NUMBEROFTEXTOUTPUT="$2"
+            shift 2
+        ;;
+        -t)
+            OUTPUTTIMEOUT="$2"
+            shift 2
+        ;;
+        --)
+            break
+        ;;
+    esac
+done
+
+for i in `seq "$OUTPUTTIMEOUT"`
+do
+    for i in `seq "$NUMBEROFTEXTOUTPUT"`
+    do
+        echo "$text"
+    done
+    sleep $OUTPUTTIMEOUT
 done
--- task-2/ptxt2.sh
+++ task-2/ptxt2.sh
@@ -1,20 +1,34 @@
+if [ "$#" != 6 ]
+then
+    echo "Invalid argument"
+    echo "usage bash $0 -n <number of text output> -t <output timeout> -- <text to output>"
+    exit 1
+fi
+
 text="$6"
+
 while getopts n:t: c
 do
     case "$c" in
-    t)
-        sleep "$OPTARG"
+        n)
+            NUMBEROFTEXTOUTPUT="$OPTARG"
         ;;
-    n)
-        for i in `seq "$OPTARG"`
-		do
-			echo "$text"
-		done
+        t)
+            OUTPUTTIMEOUT="$OPTARG"
         ;;
-    \?)
-        echo "Invalid argument"
-		echo "usage bash $0 -n <number of text output> -t <output timeout> -- <text to output>"
-		exit 1
+        \?)
+            echo "Invalid argument"
+            echo "usage bash $0 -n <number of text output> -t <output timeout> -- <text to output>"
+            exit 1
         ;;
     esac
+done
+
+for i in `seq "$OUTPUTTIMEOUT"`
+do
+    for i in `seq "$NUMBEROFTEXTOUTPUT"`
+    do
+        echo "$text"
+    done
+    sleep $OUTPUTTIMEOUT
 done
