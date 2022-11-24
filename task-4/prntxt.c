b08288f8af09d050936f718440c8cde95e9a90d4
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-11-24 11:01:52 +0300

    Add trial program to run

--- task-4/prntxt.c
+++ task-4/prntxt.c
@@ -1,8 +1,72 @@
 #include <stdio.h>
+#include <getopt.h>
+
+struct option
+{
+    const char *name;
+    int has_arg;
+    int *flag;
+    int val;
+};
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    struct option longopts[] =
+        {
+            {.name = "advanced",
+             .has_arg = no_argument,
+             .flag = NULL,
+             .val = 'a'},
+            {.name = "base address",
+             .has_arg = required_argument,
+             .flag = NULL,
+             .val = 'b'},
+            {.name = "cursor",
+             .has_arg = required_argument,
+             .flag = NULL,
+             .val = 'c'},
+            {.name = "debug",
+             .has_arg = no_argument,
+             .flag = NULL,
+             .val = 'd'},
+            {}};
+
+    while (1)
+    {
+        int c = getopt_long(argc, argv, "ab:c::d", longopts, NULL);
+
+        if (c == -1)
+        {
+            break;
+        }
+
+        switch (c)
+        {
+        case 'a':
+            printf("option 'a'\n");
+            break;
+
+        case 'b':
+            printf("option 'b' with '%s'\n", optarg);
+            break;
+        case 'c':
+            if (optarg)
+            {
+                printf("option 'c' with '%s'\n", optarg);
+            }
+            else
+            {
+                printf("option 'c' witout argument\n");
+            }
+            break;
+        case 'd':
+            printf("option 'd'\n");
+            break;
+
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
